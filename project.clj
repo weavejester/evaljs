c9f1bc2a296adab5f1b60956bedc2926dbd8f241
James Reeves <jreeves@weavejester.com>
2012-03-04 18:37:33 +0000

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject evaljs "0.1.0-SNAPSHOT"
+(defproject evaljs "0.1.0"
   :description "Evaluate Javascript code in Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.mozilla/rhino "1.7R3"]])
